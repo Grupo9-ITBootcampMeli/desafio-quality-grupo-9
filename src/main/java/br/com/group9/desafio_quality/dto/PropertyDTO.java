2d6052f1fd8ca90362b4d514c29d42ea4135ac17
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-03-30 12:46:38 -0300

    feat: id generator on PropertyDTO

--- src/main/java/br/com/group9/desafio_quality/dto/PropertyDTO.java
+++ src/main/java/br/com/group9/desafio_quality/dto/PropertyDTO.java
@@ -1,19 +1,28 @@
 package br.com.group9.desafio_quality.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PropertyDTO {
+
+    private static Long counter = 1L;
+    public static Long generateId(){
+        return counter++;
+    }
 
     private Long id;
 
-    //
     private String propName;
-    //
     private Long districtId;
 
 
     private DistrictDTO district;
 
-    //
     private List<RoomDTO> rooms;
 }
